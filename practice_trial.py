5a3cc33be58197272e49db1e66641176990672b0
南汰翼 <taisuke.minami@neuromagic.com>
2023-12-24 18:44:46 +0900

    add

--- practice_trial.py
+++ practice_trial.py
@@ -56,14 +56,14 @@ def main():
     bucket_name = st.secrets["bucket_name"]
     data_dir = st.secrets["data_dir_1"]
     result_dir = st.secrets["result_dir"]
-    serial_code = st.secrets["serial_code_1"]
+    serial_number = st.secrets["serial_number_1"]
     
     if not "finished" in st.session_state:
         st.session_state.finished = False
         
     if st.session_state.finished:
         st.write("これで実験は終了です。ありがとうございました。")
-        st.write(f"シリアルコード1は{serial_code}です。")
+        st.write(f"シリアル番号1は{serial_number}です。")
         st.stop()
     
     data_path_list = load_data(conn, bucket_name, data_dir)
@@ -76,6 +76,52 @@ def main():
     label4_list = []
     label5_list = []
     ans_list = []
+    
+    st.markdown(
+        """
+        # 音声評価実験
+        ## 練習試行
+        
+        この度は実験にご協力いただき、誠にありがとうございます。
+        
+        **こちらは本番試行の前に行う練習のための実験です。**
+        
+        以下、実験内容の説明になりますので、ご確認の程よろしくお願い致します。
+        
+        ### 行っていただく内容
+        1. 音声サンプルの視聴
+        2. 音声の自然性、明瞭性について5段階で評価
+        
+        ### 評価項目
+        ここで、評価項目である**自然性**・**明瞭性**は以下のような観点です。
+        - **自然性**：音声がどれくらい自然なものになっているか
+        - **明瞭性**：音声がどれくらい聞き取りやすいものになっているか
+        
+        ### 評価方法
+        **自然性**・**明瞭性**それぞれの観点で5段階評価をお願いします。
+        
+        5段階評価における目安は以下の通りです。
+        1. **非常に悪い**
+        2. **悪い**
+        3. **普通**
+        4. **良い**
+        5. **非常に良い**
+        
+        ### 解答における注意事項
+        - 個人の能力を測るものではないため、直感的な解答をお願いします。
+        - 各音声サンプルは、何度聞き直していただいても構いません。
+        - 解答提出までは、何度解答を変更していただいても構いません。
+        - **ページの再読み込みを行いますと問題が並び替えられ、解答は初期化されます。解答開始から提出までは、ページを再読み込みされないようご注意ください。**
+        
+        ### 解答提出
+        1. 全ての解答が終わりましたら、ページ下部の「解答を終える」ボタンをクリックしてください。
+        2. シリアル番号が表示されますので、記録をお願いします。
+        3. 記録したシリアル番号を、クラウドワークスにて提出してください。
+        
+        それでは、よろしくお願い致します。
+        """
+    )
+    st.divider()
     
     with st.form("evaluation"):
         for i, data_path in enumerate(st.session_state.data_path_list):
